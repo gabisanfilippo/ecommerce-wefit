3ac4fc42f6367da4f410e494204743e695c1d5f2
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2024-04-03 15:10:21 -0300

    feat: empty result component

---
+++ src/components/ui/Empty/index.tsx
@@ -0,0 +1,36 @@
+import { useRouter } from "next/navigation";
+import * as S from "./style";
+import { Button } from "../Button";
+import { ImageRefresh } from "@/assets/ImageRefresh";
+
+interface IEmptyProps {
+  mode: "reload" | "back";
+}
+
+export const Empty = ({ mode }: IEmptyProps) => {
+  const router = useRouter();
+
+  const contents = {
+    reload: {
+      textButton: "Recarregar página",
+      action: () => router.refresh(),
+    },
+    back: {
+      textButton: "Voltar",
+      action: () => router.back(),
+    },
+  };
+
+  const currentContent = contents[mode];
+
+  return (
+    <S.Container>
+      <h2>Parece que não há nada por aqui :(</h2>
+      <ImageRefresh />
+      <Button
+        text={currentContent.textButton}
+        onClick={currentContent.action}
+      />
+    </S.Container>
+  );
+};
---
+++ src/components/ui/Empty/style.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+  background-color: white;
+  width: 100%;
+  padding: 64px 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 24px;
+  border-radius: 4px;
+  h2 {
+    font-size: 20px;
+    font-weight: 700;
+    line-height: 27.24px;
+    text-align: center;
+    color: #2f2e41;
+  }
+  button {
+    width: 173px;
+  }
+`;
