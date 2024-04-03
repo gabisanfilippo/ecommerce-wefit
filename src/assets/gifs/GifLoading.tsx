83e482424d152e54ab2b391f999c4a9f4680a4f1
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2024-04-03 18:45:50 -0300

    fix: props name

--- src/assets/gifs/GifLoading.tsx
+++ src/assets/gifs/GifLoading.tsx
@@ -19,9 +19,9 @@ export const GifLoading = () => {
           cy="50"
           fill="none"
           stroke="rgba(255, 255, 255, 0.089)"
-          stroke-width="6"
+          strokeWidth="6"
           r="35"
-          stroke-dasharray="164.93361431346415 56.97787143782138"
+          strokeDasharray="164.93361431346415 56.97787143782138"
         >
           <animateTransform
             attributeName="transform"
