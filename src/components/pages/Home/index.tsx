e44afdb173cfdb210ece135130c2bb3007d43e90
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2024-04-03 12:51:04 -0300

    feat: request handling

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -7,16 +7,38 @@ import { api } from "@/services/api";
 import { Product, ProductsList } from "@/types/products";
 import { useEffect, useState } from "react";
 
+interface IMoviesData {
+  isLoading: boolean;
+  data: null | Product[];
+  isError: boolean;
+}
+
 export const SearchMovies = () => {
-  const [moviesList, setMoviesList] = useState<Product[]>([]);
+  const [moviesData, setMoviesData] = useState<IMoviesData>({
+    isLoading: false,
+    data: null,
+    isError: false,
+  });
 
   useEffect(() => {
     async function getProducts() {
+      setMoviesData((prev) => ({ ...prev, isLoading: true }));
+
       try {
         const response = await api.get<ProductsList>("/products");
-        setMoviesList(response.data.products);
+        setMoviesData((prev) => ({
+          ...prev,
+          isError: false,
+          data: response.data.products,
+        }));
       } catch (error: any) {
+        setMoviesData((prev) => ({
+          ...prev,
+          isError: true,
+        }));
         throw new Error(error.message);
+      } finally {
+        setMoviesData((prev) => ({ ...prev, isLoading: false }));
       }
     }
 
@@ -27,7 +49,10 @@ export const SearchMovies = () => {
     <>
       <Input />
       <S.CardsContainer>
-        {moviesList.map((movie, index) => {
+        {(!moviesData.data || moviesData.isLoading) && <>carregando...</>}
+        {moviesData.isLoading && <>erro...</>}
+        {moviesData.data && moviesData.data.length === 0 && <>vazio</>}
+        {moviesData.data?.map((movie, index) => {
           return (
             <CardAddMovie
               movieData={movie}
