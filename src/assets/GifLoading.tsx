1636c94a2843e1fb62e7ad2111d0299d53834632
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2024-04-03 15:10:47 -0300

    feat: loading component

---
+++ src/assets/GifLoading.tsx
@@ -0,0 +1,39 @@
+export const GifLoading = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 100 100"
+      preserveAspectRatio="xMidYMid"
+      width="83"
+      height="83"
+      style={{
+        shapeRendering: "auto",
+        display: "block",
+        background: "transparent",
+      }}
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+    >
+      <g>
+        <circle
+          cx="50"
+          cy="50"
+          fill="none"
+          stroke="rgba(255, 255, 255, 0.089)"
+          stroke-width="6"
+          r="35"
+          stroke-dasharray="164.93361431346415 56.97787143782138"
+        >
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            repeatCount="indefinite"
+            dur="1s"
+            values="0 50 50;360 50 50"
+            keyTimes="0;1"
+          ></animateTransform>
+        </circle>
+        <g></g>
+      </g>
+    </svg>
+  );
+};
---
+++ src/components/ui/Loading/index.tsx
@@ -0,0 +1,10 @@
+import { GifLoading } from "@/assets/GifLoading";
+import * as S from "./style";
+
+export const Loading = () => {
+  return (
+    <S.Container>
+      <GifLoading />
+    </S.Container>
+  );
+};
