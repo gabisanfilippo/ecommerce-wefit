c3b14bf0f0c5acc88a9178fea30961a0dff590ff
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2024-04-03 16:48:30 -0300

    feat: adaptation to use on cart page

--- src/components/ui/Input/index.tsx
+++ src/components/ui/Input/index.tsx
@@ -1,15 +1,23 @@
+"use client";
+
 import { IconSearch } from "@/assets/IconSearch";
 import * as S from "./style";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "../Button";
 
 interface IInputProps {
   defaultValue?: string | null;
   onSearch?: (value: string) => void;
+  showIcon?: boolean;
 }
 
-export const Input = ({ defaultValue, onSearch }: IInputProps) => {
+export const Input = ({ defaultValue, onSearch, showIcon }: IInputProps) => {
   const [isFocused, setIsFocused] = useState(false);
+  const [value, setValue] = useState(defaultValue || "");
+
+  useEffect(() => {
+    defaultValue && setValue(defaultValue);
+  }, [defaultValue]);
 
   return (
     <S.Container>
@@ -18,18 +26,21 @@ export const Input = ({ defaultValue, onSearch }: IInputProps) => {
         name="search"
         id="search-input"
         placeholder="Buscar filme pelo nome"
-        defaultValue={defaultValue ? defaultValue : ""}
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
         onFocus={() => setIsFocused((prev) => !prev)}
         onBlur={(event) => {
           onSearch && onSearch(event.target.value);
           setIsFocused((prev) => !prev);
         }}
       />
-      <Button
-        text=""
-        bgColor="transparent"
-        icon={<IconSearch color={isFocused ? "#2F2E41" : "#CCCCCC"} />}
-      />
+      {showIcon && (
+        <Button
+          text=""
+          bgColor="transparent"
+          icon={<IconSearch color={isFocused ? "#2F2E41" : "#CCCCCC"} />}
+        />
+      )}
     </S.Container>
   );
 };
